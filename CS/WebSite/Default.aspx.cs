9feb2757e621dde3e183da2311407dd3195fe639
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page {
+    protected void gv_RowUpdating(object sender, DevExpress.Web.Data.ASPxDataUpdatingEventArgs e) {
+        ASPxGridView gridView = (ASPxGridView)sender;
+        // Note that data modifications are not allowed in online demos
+        gridView.CancelEdit();
+        e.Cancel = true;
+    }
+}
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,13 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub gv_RowUpdating(ByVal sender As Object, ByVal e As DevExpress.Web.Data.ASPxDataUpdatingEventArgs)
+		Dim gridView As ASPxGridView = CType(sender, ASPxGridView)
+		' Note that data modifications are not allowed in online demos
+		gridView.CancelEdit()
+		e.Cancel = True
+	End Sub
+End Class
